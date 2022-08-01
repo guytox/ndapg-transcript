ca770a7fc2aeb1ddecdfe60ec00fdbf4d64b0508
Torkuma Agber <torkumaagber@gmail.com>
2022-08-01 21:16:10 +0100

    minor bug

--- resources/views/includes/role-sidebar/student.blade.php
+++ resources/views/includes/role-sidebar/student.blade.php
@@ -1,11 +1,11 @@
 @role('student')
 
-<li>
+{{-- <li>
     <a href="{{ route('student.outstanding.payments') }}" class="@if (Request::is('student.outstanding.payments')) active @endif waves-effect">
         <i class="mdi mdi-tag-heart"></i>
         <span>Payment</span>
     </a>
-</li>
+</li> --}}
 
 <li>
     <a href="#" class="@if (Request::is('attendee.payment')) active @endif has-arrow waves-effect">
@@ -32,23 +32,23 @@
             </a>
         </li>
 
-        <li>
+        {{-- <li>
             <a href="#" class="@if (Request::is('attendee.payment')) active @endif waves-effect">
                 <i class="mdi mdi-tag-heart"></i>
                 <span>Activte SemesterReg</span>
             </a>
-        </li>
+        </li> --}}
 
 
 
     </ul>
 </li>
 
-<li>
+{{-- <li>
     <a href="#" class=" waves-effect">
         <i class="mdi mdi-face"></i>
         <span>Profile</span>
     </a>
-</li>
+</li> --}}
 
 @endrole
