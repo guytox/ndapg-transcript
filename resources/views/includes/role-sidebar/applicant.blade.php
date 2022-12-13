eaf70fb5ba175e3f3d6ec292f69765de99d0db6b
Torkuma Agber <torkumaagber@gmail.com>
2022-12-13 17:10:21 +0100

    reordered applicant menus to prevent error

--- resources/views/includes/role-sidebar/applicant.blade.php
+++ resources/views/includes/role-sidebar/applicant.blade.php
@@ -1,6 +1,36 @@
 @role('applicant')
 
 <li>
+    <a href="#" class="has-arrow waves-effect">
+        <i class="mdi mdi-face"></i>
+        <span>Profile</span>
+    </a>
+
+    <ul>
+        <li>
+            <a href="#" class="@if (Request::is('applicant/profile/*')) active @endif waves-effect">
+                <i class="mdi mdi-face-recognition"></i>
+                <span>Biodata</span>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('applicant.profile.contact_details') }}" class="@if (Request::is('applicant.profile.contact_details')) active @endif waves-effect">
+                <i class="mdi mdi-face-outline"></i>
+                <span>Contact Details</span>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('applicant.profile.personal_details') }}" class="@if (Request::is('applicant.profile.personal_details')) active @endif waves-effect">
+                <i class="mdi mdi-face-profile"></i>
+                <span>Personal Details</span>
+            </a>
+        </li>
+
+    </ul>
+</li>
+
+
+<li>
     <a href="#" class="has-arrow @if (Request::is('application.referee')) active @endif waves-effect">
         <i class="mdi mdi-school-outline"></i>
         <span>Academics</span>
