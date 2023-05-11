56cc440beecc01c5dbc9ba9ff8baae93a8396fee
Torkuma Agber <torkumaagber@gmail.com>
2023-05-11 09:57:44 +0100

    stopped process to fix bug

--- app/Http/Controllers/AdmissionProcessingController.php
+++ app/Http/Controllers/AdmissionProcessingController.php
@@ -677,6 +677,8 @@ class AdmissionProcessingController extends Controller
     }
 
     public function beginFresherFeePayment($id){
+
+        return back()->with('info', "please refresh and try again");
         #first get the student
         $appData = ApplicantAdmissionRequest::where('uid', $id)->first();
         #next get the associated first tuition fee
