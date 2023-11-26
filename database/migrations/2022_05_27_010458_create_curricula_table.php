259b6aa0d59930a845bf4a4d8c81bf44878c8c13
Torkuma Agber <torkumaagber@gmail.com>
2023-11-26 22:06:17 +0100

    added study year to curricula

--- database/migrations/2022_05_27_010458_create_curricula_table.php
+++ database/migrations/2022_05_27_010458_create_curricula_table.php
@@ -20,11 +20,12 @@ class CreateCurriculaTable extends Migration
             $table->string('semester');
             $table->string('uid');
             $table->integer('studyLevel');
+            $table->integer('studyYear');
             $table->integer('minRegCredits');
             $table->integer('maxRegCredits');
             $table->integer('numOfCourses')->default(0);
             $table->boolean('active')->default(1);
-            $table->unique(array('programs_id', 'semester','studyLevel'));
+            $table->unique(array('programs_id', 'semester','studyLevel','studyYear'),'curricula_dup_check');
             $table->timestamps();
         });
     }
